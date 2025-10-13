f5b9c098946c8861384899804031ae124d077488
madinabolat <madinabolat01@gmail.com>
2025-10-12 22:25:09 -0400

    python hw

---
+++ fundamentals-python/week_2_project_tip_calculator.py
@@ -0,0 +1,3 @@
+# Basic Tip Calculator. Write a program that asks for the total bill amount and the tip percentage. It should then calculate and
+# display the tip amount and the total bill including the tip.
+# Advanced: You can also ask for a name (using input()) and print the total amount with that name in it.
