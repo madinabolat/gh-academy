748280f99722803e3f132667d468a9136b475699
madinabolat <madinabolat01@gmail.com>
2025-10-16 20:12:03 -0400

    hw3 part 1

--- fundamentals-python/week_2_project_tip_calculator.py
+++ fundamentals-python/week_2_project_tip_calculator.py
@@ -1,3 +1,12 @@
 # Basic Tip Calculator. Write a program that asks for the total bill amount and the tip percentage. It should then calculate and
 # display the tip amount and the total bill including the tip.
 # Advanced: You can also ask for a name (using input()) and print the total amount with that name in it.
+
+print("Hello! Let's calculate your tip. Please enter your name.")
+name = input()
+print("Hello, %s! Please enter total bill amount." %name)
+bill = float(input())
+print("Great! Now enter the tip percentage (format: 0.05 for 5%)")
+tip_percent = float(input())
+tip = bill * tip_percent
+print("%s, your tip amount is %0.2f" %(name, tip))
--- fundamentals-python/week_3_1.py
+++ fundamentals-python/week_3_1.py
@@ -24,8 +24,11 @@
 # So donuts(5) returns 'Number of donuts: 5'
 # and donuts(23) returns 'Number of donuts: many'
 def donuts(count):
-  # +++your code here+++
-  return
+  if count < 10:
+    return 'Number of donuts: %d' %count
+  else:
+    return 'Number of donuts: many'
+
 
 
 # B. both_ends
@@ -34,8 +37,10 @@ def donuts(count):
 # so 'spring' yields 'spng'. However, if the string length
 # is less than 2, return instead the empty string.
 def both_ends(s):
-  # +++your code here+++
-  return
+  if len(s) < 2: 
+    return ""
+  else: 
+    return s[0:2] + s[-2:] 
 
 
 # C. fix_start
@@ -48,8 +53,8 @@ def both_ends(s):
 # Hint: s.replace(stra, strb) returns a version of string s
 # where all instances of stra have been replaced by strb.
 def fix_start(s):
-  # +++your code here+++
-  return
+  new_string = s[1:]
+  return s[0]+new_string.replace(s[0],"*")
 
 
 # D. MixUp
@@ -60,8 +65,11 @@ def fix_start(s):
 #   'dog', 'dinner' -> 'dig donner'
 # Assume a and b are length 2 or more.
 def mix_up(a, b):
-  # +++your code here+++
-  return
+  a_first_two = a[0:2]
+  b_first_two = b[0:2]
+  a = b_first_two + a[2:]
+  b = a_first_two + b[2:]
+  return ("%s %s" %(a,b))
 
 
 # Provided simple test() function used in main() to print
