c11f23b34dd7c407a4ae410f7bd403065797ff69
madinabolat <madinabolat01@gmail.com>
2025-10-26 20:19:58 -0400

    python folder restructure

---
+++ fundamentals-python/week-5/test_number_guessing_game.py
@@ -0,0 +1,8 @@
+import unittest
+import week_5_number_guessing_game
+
+class test(unittest.TestCase):
+    def test_correct_guess(self):
+        i = 5
+        guess = 5
+        assertEquals(i, guess)
