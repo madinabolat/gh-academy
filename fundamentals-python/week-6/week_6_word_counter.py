44763b09150bdc790433241aa4dcf4df3c24b8bd
madinabolat <madinabolat01@gmail.com>
2025-10-29 23:37:18 -0400

    Create week_6_word_counter.py

---
+++ fundamentals-python/week-6/week_6_word_counter.py
@@ -0,0 +1,3 @@
+# Simple Word Counter. 
+# Write a program that asks the user for a sentence. 
+# The program should then count the occurrences of each word in the sentence and display the result as a dictionary.
