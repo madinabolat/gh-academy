7b24ded7d017b1175845096d9f8885a4fdf217d9
madinabolat <madinabolat01@gmail.com>
2025-11-02 22:50:16 -0500

    python week 6 hw

--- fundamentals-python/week-6/week_6_word_counter.py
+++ fundamentals-python/week-6/week_6_word_counter.py
@@ -1,3 +1,16 @@
 # Simple Word Counter. 
 # Write a program that asks the user for a sentence. 
 # The program should then count the occurrences of each word in the sentence and display the result as a dictionary.
+
+print("Please enter your sentence.")
+user_input = input()
+
+words = user_input.split()
+word_counter = {}
+for word in words:
+    if word in word_counter:
+        word_counter[word] += 1
+    else:
+        word_counter[word] = 1
+
+print(word_counter)
