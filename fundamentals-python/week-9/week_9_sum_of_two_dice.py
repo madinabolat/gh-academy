c29c09169bb38326e42908e3a00d833c0bd713be
madinabolat <madinabolat01@gmail.com>
2025-12-01 11:11:03 -0500

    Update week_9_sum_of_two_dice.py

--- fundamentals-python/week-9/week_9_sum_of_two_dice.py
+++ fundamentals-python/week-9/week_9_sum_of_two_dice.py
@@ -4,20 +4,17 @@
 import random
 
 def roll_dice_sum():
-    dice1 = random.randint(1,6)
-    dice2 = random.randint(1,6)
-    return dice1 + dice2
+    die1 = random.randint(1,6)
+    die2 = random.randint(1,6)
+    return die1 + die2
 
 print("Enter the number of times you want to roll the dice")
 n = int(input())
 
 dice_sum_frequencies = {}
+
 for i in range(n):
     dice_sum = roll_dice_sum()
-    # if dice_sum in dice_sum_frequencies:
-    #     dice_sum_frequencies[dice_sum] += 1
-    # else:
-    #     dice_sum_frequencies.update({dice_sum: 1})
     dice_sum_frequencies[dice_sum] = dice_sum_frequencies.get(dice_sum,0) + 1
 
 print(dice_sum_frequencies)
