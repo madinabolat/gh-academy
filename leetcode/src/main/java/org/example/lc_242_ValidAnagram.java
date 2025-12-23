f564424da785c95062dc63c4ee35a577dd139013
madinabolat <madinabolat01@gmail.com>
2025-12-22 22:48:28 -0500

    Update lc_242_ValidAnagram.java

--- leetcode/src/main/java/org/example/lc_242_ValidAnagram.java
+++ leetcode/src/main/java/org/example/lc_242_ValidAnagram.java
@@ -8,24 +8,18 @@ public class lc_242_ValidAnagram {
         char[] tChars = t.toCharArray();
         Arrays.sort(sChars);
         Arrays.sort(tChars);
-
-//        if (sChars.length != tChars.length){
-//            return false;
-//        }
-//        for (int i = 0; i < sChars.length; i++){
-//            if (sChars[i]!= tChars[i]){
-//                return false;
-//            }
-//        }
-//        return true;
-
         return Arrays.equals(sChars, tChars);
+    }
+
+    public static boolean isAnagramFrequencyMethod(String s, String t){
+        int[] freq = new int[26];
+
+        return false;
     }
 
     public static void main(String[] args) {
         String s1 = "car";
         String t1 = "rat";
-
 
         String s2 = "anagram";
         String t2 = "nagaram";
