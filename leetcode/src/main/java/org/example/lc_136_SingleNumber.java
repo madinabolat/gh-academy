43709c2912d55986f267d03837a1f81e23906438
madinabolat <madinabolat01@gmail.com>
2025-12-27 22:05:33 -0500

    Create lc_136_SingleNumber.java

---
+++ leetcode/src/main/java/org/example/lc_136_SingleNumber.java
@@ -0,0 +1,25 @@
+package org.example;
+
+public class lc_136_SingleNumber {
+    public static int singleNumberBruteForce(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n; i++){
+            for (int j = 1; i < n; i++){
+
+            }
+
+        }
+
+
+        return 0;
+
+    }
+    public static void main(String[] args) {
+        int[] nums1 = {2,2,1};
+        int[] nums2 = {4,1,2,1,2};
+        System.out.println(singleNumber(nums1));
+        System.out.println(singleNumber(nums2));
+
+
+    }
+}
