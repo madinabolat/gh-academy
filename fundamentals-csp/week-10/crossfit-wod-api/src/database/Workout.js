494cd252067b6443a070a800310d19d0ffa0bb7a
madinabolat <madinabolat01@gmail.com>
2026-01-07 20:44:49 -0500

    updates

--- fundamentals-csp/week-10/crossfit-wod-api/src/database/Workout.js
+++ fundamentals-csp/week-10/crossfit-wod-api/src/database/Workout.js
@@ -6,7 +6,15 @@ const getAllWorkouts = () => {
 }
 
 const createNewWorkout = (newWorkout) => {
-    //check if already added
+    const isAlreadyAdded = DB.workouts.findIndex(
+        (workout) => workout.name === newWorkout.name
+    ) > -1; 
+    //findIndex returns an index which the argument function satisfies
+    //if no index found returns -1
+    //(workout) => workout.name is a for loop for each workout in DB.workouts
+    if (isAlreadyAdded){
+        return;
+    }
     DB.workouts.push(newWorkout);
     saveToDatabase(DB);
     return newWorkout;
--- fundamentals-csp/week-10/crossfit-wod-api/src/services/workoutService.js
+++ fundamentals-csp/week-10/crossfit-wod-api/src/services/workoutService.js
@@ -1,4 +1,5 @@
 const Workout = require("../database/Workout");
+const { v4 : uuid } = require("uuid");
 
 const getAllWorkouts = () => {
     const allWorkouts = Workout.getAllWorkouts();
@@ -9,9 +10,15 @@ const getOneWorkout = () => {
     return;
 };
 
-const createNewWorkout = (body) => {
-    // Workout.createNewWorkout(body);
-    return;
+const createNewWorkout = (newWorkout) => {
+    const workoutToInsert = {
+        ...newWorkout, //spread operator
+        id: uuid(),
+        createdAt: new Date().toLocaleString("en-US", { timeZone: "UTC"}),
+        updatedAt: new Date().toLocaleString("en-US", { timeZone: "UTC"}),
+    };
+    const createdWorkout = Workout.createNewWorkout(workoutToInsert);
+    return createdWorkout;
 };
 
 const updateOneWorkout = () => {
