e7dbe37e5d22c24d75e927c8ec299e2c24fe76ac
madinabolat <madinabolat01@gmail.com>
2026-02-11 21:12:14 -0500

    matris multiplication

--- csp/week-18/src/main/java/org/example/Main.java
+++ csp/week-18/src/main/java/org/example/Main.java
@@ -8,22 +8,52 @@ public class Main {
         int l = 2;
         int[][] matrix1 = new int[m][n];
         int[][] matrix2 = new int[n][l];
+        int[][] matrix3 = new int[m][l];
 
 
         for (int i = 0; i < m; i++){
             for (int j = 0; j < n; j++){
                 matrix1[i][j] = (int) (Math.random()*100);
+                for (int k = 0; k < l; k++){
+                    matrix2[i][j] = (int) (Math.random()*100);
+                }
+            }
+        }
+
+        System.out.println("Matrix 1");
+        for (int i = 0; i < m; i++){
+            for (int j = 0; j < n; j++){
                 System.out.print(matrix1[i][j] + " ");
             }
             System.out.println();
         }
 
+        System.out.println("Matrix 2");
         for (int i = 0; i < n; i++){
             for (int j = 0; j < l; j++){
-                matrix2[i][j] = (int) (Math.random()*100);
                 System.out.print(matrix2[i][j] + " ");
             }
             System.out.println();
         }
+
+        //m x n
+        //n x l
+        //result: m x l
+        for (int i = 0; i < m; i++){
+            for (int k = 0; k < l; k++){
+                for (int j = 0; j < n; j++) {
+                    matrix3[i][k] += matrix1[i][j] * matrix2[j][k];
+                }
+            }
+        }
+
+
+        for (int i = 0; i < m; i++){
+            for (int j = 0; j < l; j++){
+                System.out.print(matrix3[i][j] + " ");
+            }
+            System.out.println();
+        }
+
     }
 }
